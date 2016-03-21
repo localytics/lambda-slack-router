2dd0ef4e0e1cad22206dd3809002d1f683dc7dcf
Kevin Deisz <kevin.deisz@gmail.com>
2016-03-21 13:35:36 -0400

    Support multiword commands
    Previously the router wouldn't support multiword commands because
    it would think they were arguments.

--- index.js
+++ index.js
@@ -3,20 +3,14 @@
 var ArgParser = require('./lib/arg-parser');
 var qs = require('qs');
 
-var Utils = {
-  buildResponse: function (response_type, response) {
-    var modifiedResponse = response;
-    if (typeof response === 'string') {
-      return { response_type: response_type, text: response };
-    }
-    modifiedResponse.response_type = response_type;
-    return modifiedResponse;
-  },
-
-  splitCommand: function (command) {
-    var split = command.split(' ');
-    return { commandName: split[0], args: split.slice(1) };
+// build a response to return to Slack
+var buildResponse = function (response_type, response) {
+  var modifiedResponse = response;
+  if (typeof response === 'string') {
+    return { response_type: response_type, text: response };
   }
+  modifiedResponse.response_type = response_type;
+  return modifiedResponse;
 };
 
 // wraps logic around routing
@@ -61,12 +55,12 @@ SlackBot.prototype.callCommand = function (commandName, options, callback) {
 
 // respond to the whole channel
 SlackBot.prototype.inChannelResponse = function (response) {
-  return Utils.buildResponse('in_channel', response);
+  return buildResponse('in_channel', response);
 };
 
 // respond to just the requesting user
 SlackBot.prototype.ephemeralResponse = function (response) {
-  return Utils.buildResponse('ephemeral', response);
+  return buildResponse('ephemeral', response);
 };
 
 // respond with a usage message
@@ -88,25 +82,41 @@ SlackBot.prototype.help = function (options, callback) {
   }));
 };
 
-// control the flow of queries from slack
+/**
+ * Find a command to match the given payload for "one two three", looks for matches for:
+ * - "one two three"
+ * - "one two"
+ * - "one"
+ */
+SlackBot.prototype.findCommand = function (payload) {
+  var splitPayload = payload.split(' ');
+  var commandName;
+  var commandNameLength;
+
+  for (commandNameLength = payload.length - 1; commandNameLength > 0; commandNameLength--) {
+    commandName = splitPayload.slice(0, commandNameLength).join(' ');
+    if (this.commands.hasOwnProperty(commandName)) {
+      return { commandName: commandName, args: splitPayload.slice(commandNameLength) };
+    }
+  }
+  return { commandName: 'help' };
+};
+
+// control the flow of queries from Slack
 SlackBot.prototype.buildRouter = function () {
   return function (event, context) {
     var body = qs.parse(event.body);
     var token = this.config.token;
-    var split;
+    var foundCommand;
 
     if (!body.token || body.token !== token) {
       return context.done(this.ephemeralResponse('Invalid Slack token'));
     }
 
-    split = Utils.splitCommand(body.text);
-    if (split.commandName === 'help' || !this.commands.hasOwnProperty(split.commandName)) {
-      return this.help({}, context.done);
-    }
-
-    return this.callCommand(split.commandName, {
+    foundCommand = this.findCommand(body.text);
+    return this.callCommand(foundCommand.commandName, {
       userName: body.user_name,
-      args: split.args
+      args: foundCommand.args
     }, context.done);
   }.bind(this);
 };
